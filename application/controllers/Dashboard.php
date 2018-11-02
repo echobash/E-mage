402a65354d7b07207b789234b9e3ed6e27b55e13
echobash <anwarali377@gmail.com>
2018-11-02 13:08:18 +0530

    read user data

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -16,7 +16,16 @@ class Dashboard extends CI_Controller {
 	}
 
 	public function registerUser()
-	{
+	{	
+		$data=array(
+		'first_name'=>$this->input->post('firstname'),
+		'last_name'=>$this->input->post('lastname'),
+		'email'=>$this->input->post('email'),
+		'password'=>$this->input->post('password')
+		);
+		echo "<pre>";print_r($data);
+		//'user_id', 'first_name', 'last_name', 'email', 'active', 'password'
 		$this->signup_model->addUser();
+
 	}
 }
